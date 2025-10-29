7a28f3a156998f65ef3b4454289fb6d2b9e65761
Sandra Ferrer <sandraferrer639@gmail.com>
2025-10-29 13:27:45 +0100

    Modificación del portfolio

--- script.js
+++ script.js
@@ -35,3 +35,25 @@ navLinks.forEach(link => {
     }
   });
 });
+
+
+// LIGHTBOX PARA IMÁGENES
+const images = document.querySelectorAll('.clickable');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.querySelector('.lightbox-img');
+const closeBtn = document.querySelector('.lightbox .close');
+
+images.forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = 'flex';
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+    if(e.target === lightbox) lightbox.style.display = 'none';
+});
