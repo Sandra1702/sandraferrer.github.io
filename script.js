89e4822a0cc663fd1da61e9b5c0d06f9945fe665
Sandra Ferrer <sandraferrer639@gmail.com>
2025-11-13 13:45:41 +0100

    Actualizar portfolio: nueva seccion en proyectos

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
+//Selecciona todas las secciones section
 const sections = document.querySelectorAll("section");
+//Selecciona todos los enlaces dentro del menu de navegación
 const navLinks = document.querySelectorAll("nav a");
+//Selecciona el boton hamburguesa para el menu movil
 const hamburger = document.querySelector(".hamburger");
+
+//Selecciona el elemento <nav> completo
 const nav = document.querySelector("nav");
 
 // Scroll: color activo menú
 window.addEventListener("scroll", () => {
-  let current = "";
+  let current = "";//Guarda el id de la sección actual visible
+
+  //Recorre todas las secciones del documento
   sections.forEach((section) => {
     const sectionTop = section.offsetTop - 80;
+    // Posición superior de la sección menos 80px (para compensar la navbar)
+
+    // Si el scroll vertical actual ha pasado la parte superior de la sección:
     if (scrollY >= sectionTop) {
-      current = section.getAttribute("id");
+      current = section.getAttribute("id");// Guarda el id de esa sección (por ejemplo, "sobre-mi")
     }
   });
+
+  //Recorre todos los enlaces del menu de navegación
   navLinks.forEach((a) => {
-    a.classList.remove("active");
+    a.classList.remove("active");// Quita la clase "active" de todos los enlaces
+    // Si el enlace contiene el id de la sección actual en su href...
     if (a.getAttribute("href").includes(current)) {
-      a.classList.add("active");
+      a.classList.add("active");//...añade la clase "active" solo a ese enlace
     }
   });
 });
@@ -43,17 +56,20 @@ const lightbox = document.getElementById('lightbox');
 const lightboxImg = document.querySelector('.lightbox-img');
 const closeBtn = document.querySelector('.lightbox .close');
 
+//Cuando se hace clic en una imagen 
 images.forEach(img => {
     img.addEventListener('click', () => {
         lightbox.style.display = 'flex';
-        lightboxImg.src = img.src;
+        lightboxImg.src = img.src;//Pone en grande la misma imagen
     });
 });
 
+//Cuando se hace clic en la x se oculta el lightbox
 closeBtn.addEventListener('click', () => {
     lightbox.style.display = 'none';
 });
 
+//Si el usuario hace click fuera de la imagen se oculta el lightbox
 lightbox.addEventListener('click', (e) => {
     if(e.target === lightbox) lightbox.style.display = 'none';
 });
